1b63e0ba52cfa50521238ce12c9d20aedcbb71e8
Luis-clr <logxpbr@gmail.com>
2024-06-03 16:57:48 -0300

    Arquivos basicos do projeto

---
+++ README.md
@@ -0,0 +1,7 @@
+# ConsultaFacil
+Sistema para agendamento e gerenciamento de consultas médicas.
+
+
+.NET: Backend para lógica de agendamentos e perfil de pacientes.
+API: Exposição de endpoints para criar, alterar e cancelar consultas.
+Mensageria: RabbitMQ para notificações de confirmação de consulta e lembretes.
---
+++ Tipos de consultas para o banco de dados.sql
@@ -0,0 +1,24 @@
+Algums tipos de consultas para ser inseridas 
+
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaDeRotina");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaDeEmergencia");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaDeSeguimento");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaPreventiva");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaPediatrica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaGinecologica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaObstetrica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaPsiquiatrica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaDermatologica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaCardiologica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaOrtopedica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaOftalmologica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaOdontologica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaEndocrinologica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaNutricional");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaNeurologica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaUrologica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaGastroenterologica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaReumatologica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaFisioterapeutica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaAlergologica");
+INSERT INTO tipos_consultas (tipo) VALUES ("ConsultaOtorrinolaringologica");
