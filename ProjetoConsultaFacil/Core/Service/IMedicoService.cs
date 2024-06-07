b740a4f2f6c0499661ff792cb569c76c1421e8ce
Luis-clr <logxpbr@gmail.com>
2024-06-07 17:07:36 -0300

    Core atualized

---
+++ ProjetoConsultaFacil/Core/Service/IMedicoService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IMedicoService
+    {
+        int Inserir(TbMedico medico);
+        void Editar(TbMedico medico);
+        TbMedico Obter(int id);
+        void Remove(TbMedico id);
+
+        IEnumerable<TbMedico> ObterPorNome(string nome);
+        IEnumerable<TbMedico> ObterTodos();
+
+    }
+}
