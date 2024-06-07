0357ee62cc622e0c2ebbd7b012ed51d04a89eeae
Luis-clr <logxpbr@gmail.com>
2024-06-07 14:04:09 -0300

    DTo e pasta Service

--- ProjetoConsultaFacil/ConsultaFacilWeb/obj/Debug/net8.0/ConsultaFacilWeb.AssemblyInfo.cs
+++ ProjetoConsultaFacil/ConsultaFacilWeb/obj/Debug/net8.0/ConsultaFacilWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsultaFacilWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54a56dfe323f528e6c3cc551dc5aa0733d26eedc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a4a8e3d2f2c5ffa72bfd809206044fa616984f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsultaFacilWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsultaFacilWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
